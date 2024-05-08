9dd82d0170cc50ee3b84372df221cd4c1ed44a30
c8c36 <bustion047@yahoo.com>
2024-05-07 22:49:30 -0300

    Updated dataloader and baseline model

--- dataloader.py
+++ dataloader.py
@@ -1,27 +1,64 @@
-import torch
-from torch.utils.data import Dataset, IterableDataset
-from load_data import Loader
+from typing import Optional
+from datasets import load_dataset, DownloadMode
+import json
+import os
+
+def save_loaded_dataset(cfg_file: dict, loaded_dataset_name: str, cached_dataset):
+    for split in cached_dataset.keys():
+        cached_dataset[split].to_csv(
+            os.path.join(cfg_file["datasets_directory"], cfg_file["datasets"][loaded_dataset_name]["name"], f"{split}")
+        )
 
 
-# TODO: Design custom map dataset with __len__ and __getitem__(id)
-class CustomMapDataset(Dataset):
-    def __init__(self):
-        raise NotImplementedError("")
+def load_gigaword():
+    with open("cfg.json") as f:
+        cfg_file = json.load(f)
+        download_mode = DownloadMode.FORCE_REDOWNLOAD if \
+            cfg_file["datasets"]["gigaword"]["force_download"] else DownloadMode.REUSE_DATASET_IF_EXISTS
 
-    def __len__(self):
-        raise NotImplementedError("")
+        num_proc = None if cfg_file["datasets"]["gigaword"]["num_workers"] == 0 \
+            else cfg_file["datasets"]["gigaword"]["nuw_workers"]
 
-    def __getitem__(self, index):
-        raise NotImplementedError("")
+        dataset = load_dataset(path=cfg_file["datasets"]["gigaword"]["name"],
+                               download_mode=download_mode,
+                               num_proc=num_proc,
+                               trust_remote_code=cfg_file["datasets"]["gigaword"]["trust_remote_code"],
+                               cache_dir="{}/{}/cache".format(cfg_file["datasets_directory"],
+                                                              cfg_file["datasets"]["gigaword"]["name"]),
+                               streaming=cfg_file["datasets"]["gigaword"]["stream"]
+                               )
+    return dataset
 
 
-# TODO: Design the iterable dataset wrapper
-class CustomIterableDataset(IterableDataset):
-    def __init__(self):
-        raise NotImplementedError("")
+supported_datasets = {
+    "gigaword": load_gigaword
+}
 
-    def __iter__(self):
-        raise NotImplementedError("")
 
-    def __next__(self):
-        raise NotImplementedError("")
+def build_torch_dataset(available_dataset_name: str, split: str, transform: Optional[list] = None,
+                        target_transform: Optional[list] = None):
+    torch_dataset = supported_datasets[available_dataset_name]()[split].set_format(type="torch")
+    return torch_dataset
+
+
+if __name__ == "__main__":
+    with (open("cfg.json") as file):
+        config = json.load(file)
+
+        if not config["datasets_directory"] in os.listdir():
+            os.mkdir(config["datasets_directory"])
+
+        good_val = []
+        failed_val = []
+        for dataset_name in config["load_data_validate"]:
+            try:
+                loaded_dataset = supported_datasets[dataset_name]()
+                if config["load_data_create_csv"]:
+                    save_loaded_dataset(config, dataset_name, loaded_dataset)
+                good_val.append(dataset_name)
+            except Exception as e:
+                failed_val.append("{}. Reason: {}".format(dataset_name, e.__str__()))
+
+    print("Status:")
+    print("Successfully loaded: {}".format(", ".join(good_val)))
+    print("Failed to load: {}".format("\n".join(failed_val)))
--- model.py
+++ model.py
@@ -1,11 +1,136 @@
+from typing import Optional
 import torch
 from torch import nn
+import math
 
 
-# TODO: According to the plan this is going to be the base model
-class Model(nn.Module):
-    def __init__(self):
-        super().__init__()
+def generate_decoder_mask(size: int, device: str):
+    return torch.triu(torch.ones(size, size) * float('-inf'), diagonal=1).to(device)
+
+
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, model_dim: int, max_len: int):
+        super(SinusoidalPositionalEncoding, self).__init__()
+        self.encoding = torch.zeros(max_len, model_dim)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, model_dim, 2).float() * -(math.log(10000.0) / model_dim))
+
+        self.encoding[:, 0::2] = torch.sin(position * div_term)
+        self.encoding[:, 1::2] = torch.cos(position * div_term)
+        self.encoding = self.encoding.unsqueeze(0).transpose(0, 1)  # [max_len, 1, model_dim]
+        self.register_buffer('pe', self.encoding)
 
     def forward(self, x):
-        raise NotImplementedError("")
+        x = x + self.pe[:x.size(0), :]
+        return x
+
+
+class LearnablePositionalEncoding(nn.Module):
+    def __init__(self, model_dim: int, max_len: int):
+        super(LearnablePositionalEncoding, self).__init__()
+        self.pos_embed = nn.Embedding(max_len, model_dim)
+
+    def forward(self, x: torch.Tensor):
+        seq_len, batch_size = x.size()
+        pos_idx = torch.arange(0, seq_len, dtype=torch.long, device=x.device).unsqueeze(1).repeat(1, batch_size)
+        return x + self.pos_embed(pos_idx)
+
+
+class CustomEmbedding(nn.Module):
+    def __init__(self,
+                 vocab_size: int,
+                 model_dim: int,
+                 max_len: int,
+                 learnable_pos_embeddings: Optional[bool] = False,
+                 dropout_prob: Optional[float] = 0.1):
+        super(CustomEmbedding, self).__init__()
+        self.token_embed = nn.Embedding(vocab_size, model_dim)
+        if learnable_pos_embeddings:
+            self.pos_embed = LearnablePositionalEncoding(model_dim, max_len)
+        else:
+            self.pos_embed = SinusoidalPositionalEncoding(model_dim, max_len)
+
+        self.dropout = nn.Dropout(dropout_prob) if dropout_prob != 0.0 else nn.Identity()
+
+    def forward(self, x):
+        x = self.token_embed(x)
+        x = self.pos_embed(x)
+        return self.dropout(x)
+
+
+class BaseTransformerModel(nn.Module):
+    def __init__(self,
+                 model_dim: Optional[int] = 512,
+                 inner_ff_dim: Optional[int] = 1024,
+                 n_encoders: Optional[int] = 6,
+                 n_decoders: Optional[int] = 6,
+                 n_heads: Optional[int] = 8,
+                 dropout_prob: Optional[float] = 0.1,
+                 batch_first: Optional[bool] = True
+                 ):
+        super(BaseTransformerModel, self).__init__()
+        self.model_dim = model_dim
+        self.inner_ff_dim = inner_ff_dim
+        self.encoders_stack = nn.ModuleList([])
+        for i in range(n_encoders):
+            self.encoders_stack.append(nn.TransformerEncoderLayer(model_dim, n_heads, inner_ff_dim, dropout_prob,
+                                                                  batch_first=batch_first))
+
+        self.decoders_stack = nn.ModuleList([])
+        for i in range(n_decoders):
+            self.decoders_stack.append(nn.TransformerDecoderLayer(model_dim, n_heads, inner_ff_dim, dropout_prob,
+                                                                  batch_first=batch_first))
+
+    def encode(self,
+               src: torch.Tensor,
+               src_attn_mask: Optional[torch.Tensor] = None,
+               src_padding_mask: Optional[torch.Tensor] = None,
+               is_src_attn_mask_causal: Optional[bool] = False
+               ):
+        memory = src
+        for encoder in self.encoders_stack:
+            memory = encoder(memory, src_attn_mask, src_padding_mask, is_src_attn_mask_causal)
+        return memory
+
+    def decode(self,
+               tgt: torch.Tensor,
+               memory: torch.Tensor,
+               tgt_attn_mask: Optional[torch.Tensor] = None,
+               memory_attn_mask: Optional[torch.Tensor] = None,
+               tgt_padding_mask: Optional[torch.Tensor] = None,
+               memory_padding_mask: Optional[torch.Tensor] = None,
+               is_tgt_attn_mask_causal: Optional[bool] = False,
+               is_memory_attn_mask_causal: Optional[bool] = False
+               ):
+        decoder_output = tgt
+        for decoder in self.decoders_stack:
+            decoder_output = decoder(decoder_output,
+                                     memory,
+                                     tgt_attn_mask,
+                                     memory_attn_mask,
+                                     tgt_padding_mask,
+                                     memory_padding_mask,
+                                     is_tgt_attn_mask_causal,
+                                     is_memory_attn_mask_causal)
+
+        return decoder_output
+
+    def forward(self,
+                src: Optional[torch.Tensor] = None,
+                tgt: Optional[torch.Tensor] = None,
+                memory: Optional[torch.Tensor] = None,
+                src_attn_mask: Optional[torch.Tensor] = None,
+                src_padding_mask: Optional[torch.Tensor] = None,
+                is_src_attn_mask_causal: Optional[bool] = False,
+                memory_attn_mask: Optional[torch.Tensor] = None,
+                memory_padding_mask: Optional[torch.Tensor] = None,
+                is_memory_attn_mask_causal: Optional[bool] = False,
+                tgt_attn_mask: Optional[torch.Tensor] = None,
+                tgt_padding_mask: Optional[torch.Tensor] = None,
+                is_tgt_attn_mask_causal: Optional[bool] = False
+                ):
+        if (src is not None) and (tgt is None) and (memory is None):
+            return self.encode(src, src_attn_mask, src_padding_mask, is_src_attn_mask_causal)
+        elif (src is None) and (tgt is not None) and (memory is not None):
+            return self.decode(tgt, memory, tgt_attn_mask, memory_attn_mask, tgt_padding_mask, memory_padding_mask,
+                               is_tgt_attn_mask_causal, is_memory_attn_mask_causal)
