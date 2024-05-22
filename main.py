2150d8a914da5b5ed319e3c4eb197394c6f1cbbf
c8c36 <bustion047@yahoo.com>
2024-05-22 18:36:26 -0300

    Updated test main.py
    Test loop is ready and should collect statistics

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ def train_loop(
     batch_size = training_dataloader.batch_size
     # Training statistics
     current_avg = 0.
-    current_avg_n = 0
+    current_avg_n = 1
     run_avg = 0.
-    run_avg_n = 0
+    run_avg_n = 1
     for batch_idx, (doc_batch, summary_batch, doc_pad_mask, summary_pad_batch) in enumerate(training_dataloader):
         optim.zero_grad()
         # Shape: S, N
@@ -100,10 +100,11 @@ def train_loop(
             target_summary_batch.view(-1).contiguous()
         )
 
+        current_avg += (loss_val.item() - current_avg)/current_avg_n
         current_avg_n += 1
-        current_avg = current_avg + (loss_val.item() - current_avg)/current_avg_n
+
+        run_avg += (loss_val.item() - run_avg)/run_avg_n
         run_avg_n += 1
-        run_avg = run_avg + (loss_val.item() - run_avg)/run_avg_n
 
         loss_val.backward()
         nn.utils.clip_grad_norm_(model_dict.parameters(), GRAD_NORM)
@@ -116,7 +117,7 @@ def train_loop(
             print(">Number of values processed = {}".format(batch_idx * batch_size))
             print(">Progress = {:.2f}%".format(100 * (batch_idx * batch_size) / dataset_length))
             print(">Average of average loss of batch from {} batches: {}".format(current_avg, current_avg_n))
-            current_avg_n = 0
+            current_avg_n = 1
             current_avg = 0.
             print(">Online average of average loss across batches: {}".format(run_avg))
 
@@ -136,16 +137,16 @@ def test_loop(model_dict: torch.nn.ModuleDict,
         # Test statistics
         # AutoRegressive aka iterative token prediction
         run_ar_per_token_loss = 0.
-        run_ar_per_token_loss_n = 0
+        run_ar_per_token_loss_n = 1
 
         run_ar_total_loss = 0.
-        run_ar_total_loss_n = 0
+        run_ar_total_loss_n = 1
 
         # TeacherEnforced aka evaluate training procedure
         total_te_loss = 0.
-        total_te_loss_n = 0
+        total_te_loss_n = 1
         run_te_loss = 0.
-        run_te_loss_n = 0
+        run_te_loss_n = 1
         for batch_idx, (doc_batch, summary_batch, doc_pad_batch, sum_pad_batch) in enumerate(test_dataloader):
             # Sd N
             doc_batch = doc_batch.to(device=all_device)
@@ -178,10 +179,15 @@ def test_loop(model_dict: torch.nn.ModuleDict,
             )
 
             total_mask = None
-            loss_vals = []
-            print(">Decoding...")
-            teacher_enforced_loss = torch.tensor(0., device=all_device)
-
+            total_ar_loss = 0.
+            # NOTE TO FUTURE ME: I AM NOT SURE THAT THIS MAKES SENSE
+            #   BECAUSE THIS IS BASICALLY THE SAME BEHAVIOUR AS TEACHER ENFORCEMENT
+            #   IF DIFFERENT -> SOMETHING IS WRONG WITH MY UNDERSTANDING OF TEACHER ENFORCEMENT
+            # AFTER TODAY'S TRAINING, CHECK THE VALUES. IF MY UNDERSTANDING IS CORRECT,
+            #   AR AND TE LOSSES SHOULD BE SIMILAR IF NOT THE SAME
+            # OTHERWISE, EXCLUDE NUMERICAL INSTABILITY, CHECK WHAT IS PREDICTED AND HOW LOSS IS COMPUTED,
+            #   AND DOUBLE-CHECK THE CORRECTNESS OF AVERAGES
+            # REWORK: REMAKE TO KEEP THE PREDICTED TOKENS AND REPEAT THE AFOREMENTIONED CHECKS
             for i in range(input_summary_batch.shape[0]):
                 future_mask[:, i] = False
                 total_mask = sum_pad_batch.bitwise_or(future_mask)
@@ -210,34 +216,34 @@ def test_loop(model_dict: torch.nn.ModuleDict,
                     masked_target_summary_batch.view(-1).contiguous()
                 )
 
-                teacher_enforced_loss += loss_val
-                print("{}th teacher enforced loss = {}".format(i, loss_val))
-                loss_vals.append(loss_val.item())
-            print("Total teacher enforced loss = {}. Average teacher enforced loss = {} ".format(
-                teacher_enforced_loss,
-                teacher_enforced_loss/len(loss_vals)
-            ))
+                total_ar_loss += loss_val.item()
+                run_ar_per_token_loss += (loss_val.item() - run_ar_per_token_loss)/run_ar_per_token_loss_n
+                run_ar_per_token_loss_n += 1
+            run_ar_total_loss += (total_ar_loss - run_ar_total_loss)/run_ar_total_loss_n
+            run_ar_total_loss_n += 1
 
-            full_transformer_output = torch.zeros_like(target_summary_batch)
-            full_transformer_output[0, :] = SOS_token
-            autoregressive_loss = torch.tensor(0.)
-            autoregressive_loss_vals = []
-            future_mask = torch.ones(input_summary_batch.shape, device=all_device, dtype=torch.bool)
-            for i in range(input_summary_batch.shape[0]):
-                future_mask[:, i] = False
-                full_transformer_output = embedder(full_transformer_output)
-                full_transformer_output = transformer.decoder(
-                    tgt=full_transformer_output,
-                    memory=memory_batch,
-                    tgt_attn_mask=attn_mask,
-                    memory_attn_mask=None,
-                    tgt_padding_mask=future_mask,
-                    memory_padding_mask=doc_pad_batch,
-                    is_tgt_attn_mask_causal=False,
-                    is_memory_attn_mask_causal=False
-                )
+            embedded_summary = embedder(input_summary_batch)
+            decoded_features = transformer.decode(tgt=embedded_summary,
+                                                  memory=memory_batch,
+                                                  tgt_attn_mask=attn_mask,
+                                                  memory_attn_mask=None,
+                                                  tgt_padding_mask=sum_pad_batch,
+                                                  memory_padding_mask=doc_pad_batch,
+                                                  is_tgt_attn_mask_causal=False,
+                                                  is_memory_attn_mask_causal=False
+                                                  )
+            prediction = classification_head(decoded_features)
 
-                prediction = classification_head(full_transformer_output)
+            loss_val = loss_fn(
+                prediction.view(-1, prediction.shape[-1]).contiguous(),
+                target_summary_batch.view(-1).contiguous()
+            )
+
+            run_te_loss += (loss_val.item() - run_te_loss)/run_te_loss_n
+            run_te_loss_n += 1
+
+            total_te_loss += (loss_val.item() - total_te_loss)/total_te_loss_n
+            total_te_loss_n += 1
 
             if batch_idx % TEST_PRINT_BATCHES == 0:
                 print(">Status:")
@@ -245,7 +251,26 @@ def test_loop(model_dict: torch.nn.ModuleDict,
                 print(">Batch idx = {}".format(batch_idx))
                 print(">Number of values processed = {}".format(batch_idx * batch_size))
                 print(">Progress = {:.2f}%".format(100 * (batch_idx * batch_size) / dataset_length))
-                print(">Average total autoregressive loss ")
+                print(">METRICS:")
+                # Average of the average batch loss across all teacher enforced tokens
+                print(
+                    ">Teacher enforced loss across the most recent {} batches: {}".format(run_te_loss_n, run_te_loss)
+                )
+                run_te_loss = 0.
+                run_te_loss_n = 1
+                print(
+                    ">Teacher enforced loss across {} batches from start: {}".format(total_te_loss_n, total_te_loss)
+                )
+                # Average of the average batch loss per predicated tokens
+                print(
+                    ">Autoregressive loss across {} the most recent tokens: {}".format(run_ar_per_token_loss_n, run_ar_per_token_loss)
+                )
+                run_ar_per_token_loss_n = 1
+                run_ar_per_token_loss = 0.
+                # Average of the accumulated tokens batch losses
+                print(
+                    ">Autoregressive loss averaged across the accumulated batch token losses: {}".format(run_ar_total_loss)
+                )
 
 
 def custom_collate_fn(batch):
